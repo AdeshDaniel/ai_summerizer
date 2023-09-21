96dc7d8c0a6eded4dd5d76f331f6962d404b31b1
AdeshDaniel <stockhutfxcompany@gmail.com>
2023-09-21 18:42:46 +0100

    done with integrating the redux provider

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,16 +3,24 @@ import { logo } from "../assets";
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-      <nav className='flex justify-between items-center flex-col'>
+      <nav className='flex justify-between items-center mb-10 w-full pt-3'>
         <img src={logo} alt='logo' className='w-28 object-contain' />
         <button
           type='button'
-          onClick={() => window.open("https://github.com/texedev")}
+          onClick={() => window.open("https://github.com/adeshdaniel")}
           className='black_btn'
         >
           Github
         </button>
       </nav>
+      <h1 className='head_text'>
+        Summerize Articles With <br className='max-md:hidden' />
+        <span className='orange_gradient'>OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify your reading with summeries, an open source article summerizer
+        that lengthy articles into clear and concise summeries
+      </h2>
     </header>
   );
 };
